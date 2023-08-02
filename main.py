d82086c4ec7beb4ef291aa6466db607bdeee8a85
Abdul Rehman Arif <coolabdulrehman3030@gmail.com>
2023-08-02 20:39:07 +0500

    Footer and for nest loop

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ for index, rows in df.iterrows():
     pdf.set_font(family='Times', size=24, style='B')
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=rows['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for i in range(21, 282, 10):
+        pdf.line(10, i, 200, i)
 
     pdf.ln(265)
     pdf.set_font(family='Times', size=8, style='I')
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=rows['Topic'], align='R')
 
-    for i in range(rows['Pages'] - 1):
+    for j in range(rows['Pages'] - 1):
         pdf.add_page()
+
+        for r in range(21, 282, 10):
+            pdf.line(10, r, 200, r)
 
         pdf.ln(277)
         pdf.set_font(family='Times', size=8, style='I')
