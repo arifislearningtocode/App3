56f92f90676c63fa3678e0f7ddd172e4d82c5d8b
Abdul Rehman Arif <coolabdulrehman3030@gmail.com>
2023-08-02 20:26:16 +0500

    Footer and for nest loop

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+
 df = pd.read_csv('topics.csv')
 
 for index, rows in df.iterrows():
@@ -11,6 +13,19 @@ for index, rows in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=rows['Topic'], align='L', ln=1)
     pdf.line(10, 21, 200, 21)
+
+    pdf.ln(265)
+    pdf.set_font(family='Times', size=8, style='I')
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=rows['Topic'], align='R')
+
+    for i in range(rows['Pages'] - 1):
+        pdf.add_page()
+
+        pdf.ln(277)
+        pdf.set_font(family='Times', size=8, style='I')
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=rows['Topic'], align='R')
 
 pdf.output('output.pdf')
 
